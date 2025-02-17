cf5c60b26b147f46c31c7e5dac5ada3c60fbd3e7
BugHunter-X <askzenai@gmail.com>
2025-02-17 08:04:05 +0100

    Add bugSolution.rs file

---
+++ bugSolution.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut vec = Vec::new();
+    vec.push(1);
+    vec.push(2);
+    let index = 10;
+
+    // Safe way to access vector elements
+    match vec.get(index) {
+        Some(element) => println!("Element at index {}: {}", index, element),
+        None => println!("Index {} is out of bounds", index),
+    };
+} 
